bc61b7c74e492de37896a76698a793693297f638
kenkeiras <kenkeiras@gmail.com>
2013-12-05 23:35:59 +0100

    Fixed image search

--- src/webLib.erl
+++ src/webLib.erl
@@ -1,4 +1,4 @@
-% 
+%
 %% webLib.erl
 %%
 %% @author Iago Lastra Rodriguez <iago.lastra@gmail.com>
@@ -6,34 +6,29 @@
 -module(webLib).
 -export([getData/1]).
 
--include_lib("xmerl/include/xmerl.hrl"). 
+-include_lib("xmerl/include/xmerl.hrl").
 
 %Retunrs title if url = "http://www.youtube.com"
 getData(URL) ->
 	inets:start(),
 	{_, { _, _, Data}} = httpc:request(URL),
-	L1 = crawl_link(mochiweb_html:parse(Data),[]),
-	L2 = crawl_image(mochiweb_html:parse(Data),[]),
-	Links = [ V || {P, V} <- L1, P == <<"href">>],
-	Images = [ V || {P, V} <- L2, P == <<"src">>],
+        DataTree = mochiweb_html:parse(Data),
+        ATags = getTags(<<"a">>, DataTree),
+        ImgTags = getTags(<<"img">>, DataTree),
+        Links = [ V || {P, V} <- ATags, P == <<"href">>],
+	Images = [ V || {P, V} <- ImgTags, P == <<"src">>],
 	{Links, Images}.
 
 
-crawl_link({<<"a">>, Properties, Children}, LinksList) ->
-	L = lists:flatten([ crawl_link(Child, [Properties | LinksList]) || Child <- Children]),
-	L;
-crawl_link({_, _Properties, Children}, LinksList) ->
-	L = lists:flatten([ crawl_link(Child, LinksList) || Child <- Children]),
-	L;
-crawl_link(_, LinksList) -> 
-	LinksList.
+getChildrenTags(Tag, Children) ->
+        lists:flatten(lists:map(fun (Child) -> getTags(Tag, Child) end,
+                                Children)).
 
-		
-crawl_image({<<"img">>, _Properties, Children}, ImageList) ->
-	L = lists:flatten([ crawl_image(Child, [1 | ImageList]) || Child <- Children]),
-	L;
-crawl_image({_, _, Children}, ImageList) ->
-	L = lists:flatten([ crawl_image(Child, ImageList) || Child <- Children]),
-	L;
-crawl_image(_, ImageList) -> 
-	ImageList.
+getTags(Tag, {_DataTag, Properties, Children}) when Tag =:= _DataTag ->
+        [Properties | getChildrenTags(Tag, Children)];
+
+getTags(Tag, {_DataTag, _Properties, Children}) when Tag =/= _DataTag ->
+        getChildrenTags(Tag, Children);
+
+getTags(_Tag, _) ->
+        [].
