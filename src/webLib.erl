aa93f878411de8761166e0a549e0f3cdd2f8640e
kenkeiras <kenkeiras@gmail.com>
2013-12-05 23:38:10 +0100

    Keep the tab-based indentation

--- src/webLib.erl
+++ src/webLib.erl
@@ -12,22 +12,22 @@
 getData(URL) ->
 	inets:start(),
 	{_, { _, _, Data}} = httpc:request(URL),
-        DataTree = mochiweb_html:parse(Data),
-        ATags = lists:flatten(filterTags(<<"a">>, DataTree)),
-        ImgTags = lists:flatten(filterTags(<<"img">>, DataTree)),
-        Links = [ V || {P, V} <- ATags, P == <<"href">>],
+	DataTree = mochiweb_html:parse(Data),
+	ATags = lists:flatten(filterTags(<<"a">>, DataTree)),
+	ImgTags = lists:flatten(filterTags(<<"img">>, DataTree)),
+	Links = [ V || {P, V} <- ATags, P == <<"href">>],
 	Images = [ V || {P, V} <- ImgTags, P == <<"src">>],
 	{Links, Images}.
 
 
 getChildrenTags(Tag, Children) ->
-        lists:map(fun (Child) -> filterTags(Tag, Child) end, Children).
+	lists:map(fun (Child) -> filterTags(Tag, Child) end, Children).
 
 filterTags(Tag, {_DataTag, Properties, Children}) when Tag =:= _DataTag ->
-        [Properties | getChildrenTags(Tag, Children)];
+	[Properties | getChildrenTags(Tag, Children)];
 
 filterTags(Tag, {_DataTag, _Properties, Children}) when Tag =/= _DataTag ->
-        getChildrenTags(Tag, Children);
+	getChildrenTags(Tag, Children);
 
 filterTags(_Tag, _) ->
-        [].
+	[].
